c4076b6367ed5f71f3b8076b01b8a2ddfc5a5988
re7za <syd.id7@gmail.com>
2021-04-19 21:40:15 +0400

    Add FilterInputs to Updates page

--- src/pages/Updates/Updates.jsx
+++ src/pages/Updates/Updates.jsx
@@ -1,7 +1,21 @@
+import "./style.css";
 import React from "react";
 
+// Misc
+import FilterInputs from "../../components/FilterInputs";
+
 const Updates = () => {
-  return <div>This is a table</div>;
+  return (
+    <div>
+      <div className="updates-title">
+        <h2>صفحه گزارش تغییرات آگهی ها</h2>
+      </div>
+      <div className="updates-content">
+        <FilterInputs />
+        <div className="updates-table">table</div>
+      </div>
+    </div>
+  );
 };
 
 export default Updates;
